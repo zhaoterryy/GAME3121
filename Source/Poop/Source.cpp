32f36a2cad22327295261793bfd33809c3258ae4
Terry <zhao.terryy@gmail.com>
2017-09-19 11:28:10 -0400

    poop

---
+++ Source/Poop/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+#include <direct.h>
+#include <d3d12.h>
+
+#define SAFE_DELETE(p) { if (p) { delete p; (p) = NULL; } }
+
+
+int main() {
+	
+	return 0;
+}
